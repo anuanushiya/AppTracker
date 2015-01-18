7661fb2ada1a241bada4a3d08d332a4f1529cb8f
Eyal Benmoshe <ebm2626@yahoo.com>
2015-01-18 15:29:33 -0800

    fixed showhtml bugs

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -11,6 +11,7 @@ class AppsController < ApplicationController
 
 	def create 
 		@app = App.new(params.require(:app).permit(:file_name, :date_created, :location, :description, :reference))
+		@app.deploy = Deploy.new(params.require(:app).require(:deploy_attributes).permit(:platform_used, :deployment_date, :deployed_name))
 
 		if @app.save
 			redirect_to apps_path
