03d68eec2684df78b0b2fea577051b5105102be3
Eyal Benmoshe <ebm2626@yahoo.com>
2015-01-18 16:27:26 -0800

    filename and location presence true

--- app/models/app.rb
+++ app/models/app.rb
@@ -8,6 +8,9 @@ class App
   field :description, type: String
   field :reference, type: String
 
+  validates :file_name, presence: true
+  validates :location, presence: true
+
   embeds_one :deploy
   accepts_nested_attributes_for :deploy
 
