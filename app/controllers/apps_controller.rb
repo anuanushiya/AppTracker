cc78db9905a71e0bb6788cd9bd0e81455546c618
Eyal Benmoshe <ebm2626@yahoo.com>
2015-01-18 16:02:28 -0800

    edit def in controller and edit html in app

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -24,6 +24,19 @@ class AppsController < ApplicationController
 		@app = App.find(params[:id])
 	end
 
+	def edit 
+		@app = App.find(params[:id])
+	end
+
+	def update
+		@app = App.find(params[:id])
+		if @app.update_attributes(params.require(:app).permit(:file_name, :date_created, :location, :description, :reference)) && @app.deploy.update_attributes(params.require(:app).require(:deploy_attributes).permit(:platform_used, :deployment_date, :deployed_name))
+			redirect_to apps_path
+		else
+			render "edit"
+		end
+	end
+
 
 
 end
