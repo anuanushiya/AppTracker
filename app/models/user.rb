f7299187e269b44d23a0d01b138b8804eecdf863
Eyal Benmoshe <ebm2626@yahoo.com>
2015-01-20 18:11:47 -0800

    minimum 6 chars password

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :password_digest, uniqueness: true
+  validates :password, length: {minimum: 6}
+
 
   has_many :apps
 
