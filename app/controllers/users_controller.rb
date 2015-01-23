fe43fd8614eb8701b5491608a287e0b24742f766
Eyal Benmoshe <ebm2626@yahoo.com>
2015-01-22 23:38:48 -0800

    error messages in sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,11 @@ class UsersController < ApplicationController
 		@user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
 
 		if @user.save
+			
 			session[:user_id] = @user.id
 			redirect_to apps_path
 		else
+			
 			render :new
 		end
 	end
